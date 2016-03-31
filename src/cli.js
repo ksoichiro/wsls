3ae1bbea92b39b10ca750273ab10ba8ee2d0c0c1
Soichiro Kashima <soichiro.kashima@gmail.com>
2016-03-31 16:37:04 +0900

    Fix that it doesn't work without args

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 'use strict';
 
+import path from 'path';
 import { Ws } from './';
 
-let cwd = (2 < process.argv.length && process.argv[2]) || __dirname;
+let cwd = (2 < process.argv.length && process.argv[2]) || path.dirname(__dirname);
 
 let workspace = new Ws(cwd);
 workspace.retrieve().then(function() {
